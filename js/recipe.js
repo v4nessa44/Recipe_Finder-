d0bb700e71d90954cd037b9aa86539b73ce09ef8
v4nessa04 <v4nessa04@gmail.com>
2024-02-26 19:22:42 -0500

    changes to recipe and images

--- js/recipe.js
+++ js/recipe.js
@@ -5,51 +5,78 @@ let mealContainer = document.querySelector(".meal-container");
 
 let API_URL = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 
+
+const makeList = (meal)=>{
+//Make list of ingredients 
+let listOfIngredients = [];
+for(let i = 1; i<= 20; i++){
+    let strI = "strIngredient" + i;
+    let strM = "strMeasure" + i;
+    
+    let ingredient = meal[strI] + " " + meal[strM];
+    if(ingredient.trim() !="") listOfIngredients.push(ingredient)
+}
+
+return listOfIngredients;
+};
+
+const makeUI = (meal, list) => {
+     //Make the HTML for that list
+     let html = `
+     <img src="${meal.strMealThumb}"/>
+     <h1>${meal.strMeal}</h1>
+     <div class="row">
+         <p> <span>Category: </span> ${meal.strCategory}</p>
+         <p> <span>Area: </span> ${meal.strArea}</p>
+     </div>
+
+     <div class="ingredients">
+         <h3>Ingredients</h3>
+         <ul>
+             ${list.map((element)=> `<li>${element}</li>`).join("")}
+         </ul>
+     </div>
+
+     <div class="instructions">
+         <h3>Instructions</h3>
+         <p>
+             ${meal.strInstructions}
+     </div>
+     
+ </div>
+</div>
+ `;
+
+ return html;
+}
+
+
 const fetchRecipe = async(e)=>{
     e.preventDefault();
-    
-    console.log(input.value);
 
+
+    //Show search GIF 
+    let searchGIFHTML = `
+            <img src="../images/search.gif"/>
+            <h1>Search For Your Recipe</h1>
+    `
+
+    mealContainer.innerHTML=searchGIFHTML;
+    //call API
     let response = await fetch(API_URL + input.value);
     let data = await response.json();
 
-    
+    //Check data 
     if(data.meals){
         let meal = data.meals[0];
-        console.log(meal);
-    
 
-    let html = `
-        <img src="${meal.strMealThumb}"/>
-        <h1>${meal.strMeal}</h1>
-        <div class="row">
-            <p> <span>Category: </span> ${meal.strCategory}</p>
-            <p> <span>Area: </span> ${meal.strArea}</p>
-        </div>
+     //call function
+     let list = makeList(meal);
+     
+     let html = makeUI(meal, list);
+   
 
-        <div class="ingredients">
-            <h3>Ingredients</h3>
-            <ul>
-                <li>Ingredient 1</li>
-                <li>Ingredient 2</li>
-                <li>Ingredient 3</li>
-                <li>Ingredient 4</li>
-                <li>Ingredient 5</li>
-                <li>Ingredient 6</li>
-                <li>Ingredient 7</li>
-            </ul>
-        </div>
-
-        <div class="instructions">
-            <h3>Instructions</h3>
-            <p>
-                ${meal.strInstructions}
-        </div>
-        
-    </div>
-</div>
-    `
-
+    //Show the list
     mealContainer.innerHTML =html;}
 };
 
