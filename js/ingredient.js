f891954e9cdf5a9b8ea295870557936866c8e04c
Vanessa Decopin <vanedecopin@gmail.com>
2024-03-08 17:39:30 -0500

    changes

--- js/ingredient.js
+++ js/ingredient.js
@@ -1,55 +1,12 @@
 let inputFormEL = document.querySelector("form");
 let inputEL = document.querySelector("input");
-let mealContainerEL = document.querySelector(".meal-container");
+let mealContainerEL = document.querySelector(".ingredients-container");
+let closeBtn = document.querySelector(".close")
+let detailModal = document.querySelector(".detail-modal")
 
 
 let API_URL = "https://www.themealdb.com/api/json/v1/1/filter.php?i=";
 
-
-const makeList = (meal)=>{
-//Make list of ingredients 
-let listOfIngredients = [];
-for(let i = 1; i<= 20; i++){
-    let strI = "strIngredient" + i;
-    let strM = "strMeasure" + i;
-    
-    let ingredient = meal[strI] + " " + meal[strM];
-    if(ingredient.trim() !="") listOfIngredients.push(ingredient)
-}
-
-return listOfIngredients;
-};
-
-const makeUI = (meal, list) => {
-     //Make the HTML for that list
-     let html = `
-     <img src="${meal.strMealThumb}"/>
-     <h1>${meal.strMeal}</h1>
-     <div class="row">
-         <p> <span>Category: </span> ${meal.strCategory}</p>
-         <p> <span>Area: </span> ${meal.strArea}</p>
-     </div>
-
-     <div class="ingredients">
-         <h3>Ingredients</h3>
-         <ul>
-             ${list.map((element)=> `<li>${element}</li>`).join("")}
-         </ul>
-     </div>
-
-     <div class="instructions">
-         <h3>Instructions</h3>
-         <p>
-             ${meal.strInstructions}
-         </p>
-     </div>
-     
- </div>
-</div>
- `;
-
- return html;
-}
 
 
 const fetchRecipe = async(e)=>{
@@ -58,38 +15,98 @@ const fetchRecipe = async(e)=>{
 
     //Show search GIF 
     let searchGIFHTML = `
+            <div class="help">
             <img src="../images/search.gif"/>
-            <h1>Search For Your Recipe</h1>
+            <h1>Searching For Your Recipe...</h1>
+            </div>
+            
     `
 
-    mealContainerEL.innerHTML=searchGIFHTML;
+    mealContainerEL.innerHTML = searchGIFHTML;
     
     
     //call API
     let response = await fetch(API_URL + inputEL.value);
     let data = await response.json();
 
+    console.log(data.meals);
     //Check data 
     if (data.meals){
-        let meal = data.meals[0];
+        mealContainerEL.innerHTML = "";
 
-     //call function
-     let list = makeList(meal);
+        let btn = document.createElement("button");
+
+        
+
+        data.meals.map((meal)=>{
+            let html = `
+            <div class="card">
+            <img 
+            src= ${meal.strMealThumb}
+            alt="text" />
+            <h1>${meal.strMeal}</h1>
+            <button class="detail-btn" id=${meal.idMeal}>See Details</button>
+        </div>
+            `;
+
+            mealContainerEL.insertAdjacentHTML("beforeend", html);
+           
+        });
      
-     let html = makeUI(meal, list);
-   
-
-    //Show the list
-    mealContainerEL.innerHTML =html;}
-
-   else{
+    }else{
     let sorryGIFHTML = `
+            <div class="help">
             <img src="../images/sorry.gif"/>
             <h2>We are unable to find your recipe...</h2>
+            </div>
     `;
 
     mealContainerEL.innerHTML= sorryGIFHTML;
    }
 };
 
+
+function closeModal(){
+    detailModal.getElementsByClassName.display = "none";
+}
+
+
+
+function showDetails(e){
+    console.log("details", e.target);
+    if(e.target && e.target.className ==="detail-btn"){
+        let mealId = e.target.id;
+        //1. fetch meal details
+
+    let response = await fetch(
+        "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + mealId
+      );
+      let data = await response.json();
+  
+      const meal = data.meals[0];
+  
+      // show data in modal
+      let html = `
+      <img src="${meal.strMealThumb}" />
+      <h1>${meal.strMeal}</h1>
+      <div class="row">
+        <p><span>Category:</span> ${meal.strCategory}</p>
+        <p><span>Area:</span> ${meal.strArea}</p>
+      </div>
+  
+    
+      <div class="instructions">
+        <h3>Instructions</h3>
+        <p>${meal.strInstructions}</p>
+      </div>
+      `;
+      modalContent.innerHTML = html;
+      // open modal
+      detailModal.style.display = "flex";
+    }
+  }
+
+
 inputFormEL.addEventListener("submit", fetchRecipe);
+closeBtn.addEventListener("click", closeModal);
+mealContainerEL.addEventListener("click", showDetails)
